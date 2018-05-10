7cbfe27506f224702011c2b8a5499f293e796213
Andreas Schranz <andreas.schranz@technikum-wien.at>
2018-05-10 13:55:16 +0200

    also working with https

--- index.php
+++ index.php
@@ -7,7 +7,8 @@ session_start();
 $g_client = new Google_Client();
 $g_client->setClientId("127519901009-d6di364446faheeq2aaaqvpnm1dh891u.apps.googleusercontent.com");
 $g_client->setClientSecret("DzERhutnLzrSa8SoHq_2p08D");
-$g_client->setRedirectUri("http://localhost/das/google_oauth2_callback_test.php");
+$g_client->setRedirectUri("https://localhost/das/google_oauth2_callback.php");
+//$g_client->setRedirectUri("http://localhost/das/google_oauth2_callback_test.php");
 $g_client->setScopes("https://www.googleapis.com/auth/userinfo.profile");
 $auth_url = $g_client->createAuthUrl();
 
@@ -16,11 +17,6 @@ if (isset($_SESSION['access_token']) && $_SESSION['access_token'])
 {
   $g_client->setAccessToken($_SESSION['access_token']);
 }
-/*else
-{
-  $redirect_uri = 'http://' . $_SERVER['HTTP_HOST'] . '/das/google_oauth2_callback_test.php';
-  header('Location: ' . filter_var($redirect_uri, FILTER_SANITIZE_URL));
-}*/
 
 
 ?>
@@ -36,7 +32,7 @@ if (isset($_SESSION['access_token']) && $_SESSION['access_token'])
 	<body>
 		<a href="Login.php"> Login Formular </a>
 		<br/>		
-<?php echo "<a href='$auth_url'> Login with Google </a>"; ?>
+	<?php echo "<a href='$auth_url'> Login with Google </a>"; ?>
 		<br/>
 		<a href=""> Login http </a>
 	</body>
