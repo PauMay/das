80677860ce9764bc117bb8af3fed8144254f6615
Andreas Schranz <andreas.schranz@technikum-wien.at>
2018-05-10 14:06:32 +0200

    google-login: added comments

--- google_oauth2_callback.php
+++ google_oauth2_callback.php
@@ -6,36 +6,35 @@ if(!isset($_SESSION))
 	session_start();
 }
 
+// setup google client
 $g_client = new Google_Client();
 $g_client->setAuthConfigFile('../das_permitted/client_secret.json');
 $g_client->setRedirectUri('https://' . $_SERVER['HTTP_HOST'] . '/das/google_oauth2_callback.php');
 $g_client->setScopes("https://www.googleapis.com/auth/userinfo.profile");
 
+//
 $code = isset($_GET['code']) ? $_GET['code'] : NULL;
 
-if(isset($code))
+if(isset($code)) // authorization code recieved
 {
+	// exchange authorization code for access token
 	try
 	{
 		$g_client->authenticate($code);
-	} catch (Exception $e) {
-		// log exception - don't display it    
-	}
-	
-	try
-	{
-		$token = $g_client->getAccessToken();
+		$access_token = $g_client->getAccessToken();
 	} catch (Exception $e) {
 		// log exception - don't display it      
 	}
     
+	// use access token to call google API
 	try
 	{
-        $g_client->setAccessToken($token);
+        $g_client->setAccessToken($access_token);
     } catch (Exception $e){
 		// log exception - don't display it      
     }
-
+	
+	// retrieve payload (= user information - in this case)
     try
 	{
         $pay_load = $g_client->verifyIdToken();	
@@ -50,6 +49,7 @@ if(isset($code))
 
 if(isset($pay_load)){
 
+	// if login was successful, save username and redirect
 	$service = new Google_Service_Oauth2($g_client);
 	$user = $service->userinfo->get();
 	$_SESSION['g_username'] = $user->name;
