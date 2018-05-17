feb4f0ea7feb9b6e7c69de6ad557dcbe96049717
Andreas Schranz <andreas.schranz@technikum-wien.at>
2018-05-17 19:44:24 +0200

    google-login: detail-änderung

--- index.php
+++ index.php
@@ -5,8 +5,7 @@ session_start();
 
 // setup google client
 $g_client = new Google_Client();
-$g_client->setClientId("127519901009-d6di364446faheeq2aaaqvpnm1dh891u.apps.googleusercontent.com");
-$g_client->setClientSecret("DzERhutnLzrSa8SoHq_2p08D");
+$g_client->setAuthConfigFile('../das_permitted/client_secret.json');
 $g_client->setRedirectUri("https://localhost/das/google_oauth2_callback.php");
 $g_client->setScopes("https://www.googleapis.com/auth/userinfo.profile");
 $auth_url = $g_client->createAuthUrl();
