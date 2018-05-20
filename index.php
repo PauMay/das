cd27347412adb3ec70761572e290ab5ee1eb85b5
raffi287 <raffi.papst@gmail.com>
2018-05-20 13:08:38 +0200

    Index wird jetzt immer auf https weitergeleitet, damit das funktioniert müsst ihr den DocumentRoot in der httpd.conf auf htdocs/das ändern

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
 <?php
+if (!isset($_SERVER["HTTPS"]) || $_SERVER["HTTPS"] != "on") 
+	{
+        header("Location: https://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]);
+        exit;
+    }
+
+
 require_once __DIR__.'/google-api-php-client/vendor/autoload.php';
 
 // setup google client
